c4f464bed0efbb7f72bfea2e01fce4c7755a47f1
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2024-04-16 19:28:38 +0300

    feat: gh pages integration/deploy

---
+++ build/precache-manifest.c2e3d0d378583f70d80e242169f51c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94093c43affae99df0dd53c742779d40",
+    "url": "/React-Pagination-Github/index.html"
+  },
+  {
+    "revision": "0c88529f6b4840b65f5b",
+    "url": "/React-Pagination-Github/static/css/main.fa521789.chunk.css"
+  },
+  {
+    "revision": "3e92073cceabe76ae380",
+    "url": "/React-Pagination-Github/static/js/2.85ff8da7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Pagination-Github/static/js/2.85ff8da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c88529f6b4840b65f5b",
+    "url": "/React-Pagination-Github/static/js/main.819ee92b.chunk.js"
+  },
+  {
+    "revision": "e642e30357c0df5e32a8",
+    "url": "/React-Pagination-Github/static/js/runtime-main.86798c60.js"
+  }
+]);
